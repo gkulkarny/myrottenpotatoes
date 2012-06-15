733af8eedc7fc3de92988d6556b2d0e57cd0078e
saasbook <saasbook@saasbook.(none)>
2012-06-15 09:37:16 -0700

    mypotatoes initial

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,34 @@
+class MoviesController < ApplicationController
+	def index
+	  @movies = Movie.all
+        end
+        def show
+          id = params[:id]
+          @movie = Movie.find(id)
+        end
+        def new
+           # default : render 'new' template
+        end
+        def create
+          @movie = Movie.create!(params[:movie])
+          flash[:notice] = "#{@movie.title} was successfully created."
+          redirect_to movies_path
+        end
+        # in movies_controller.rb
+	def edit
+  	  @movie = Movie.find params[:id]
+	end
+
+	def update
+  	  @movie = Movie.find params[:id]
+  	  @movie.update_attributes!(params[:movie])
+  	  flash[:notice] = "#{@movie.title} was successfully updated."
+  	  redirect_to movie_path(@movie)
+        end
+        def destroy
+          @movie = Movie.find(params[:id])
+          @movie.destroy
+          flash[:notice] = "#{@movie.title} was successfully deleted."
+          redirect_to movies_path
+        end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '40b1b37d18a71a9b47b1614d13363fa1c870bfedf0677c7d00f1a20bd231669de8f5b5962ddd22924d40d386154a8d5cfe4a98a4843cfd38cc69f3e2bb25ec00'
